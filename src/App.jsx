d12f6cdbe48ddeb05a919502feb564605cbe5c8d
bettermessi <bozlaktuna5@gmail.com>
2025-04-25 09:54:08 +0300

    denemetesti
    jenkins test ediliyor

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
       <h1>Hello World</h1>
       <p>This is a simple React application.</p>
       <p>It is set up to use Vite for development.</p>
+      <p>
+        Vite is a fast build tool that provides a great developer experience.
+      </p>
+      <p>It supports hot module replacement (HMR) for instant updates.</p>
     </div>
   );
 }
