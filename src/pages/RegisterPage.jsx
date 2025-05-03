f75e8ab0e7f07efc138cdb9f5df03025a2febe7e
Celikkanat05 <114948859+Dannaka1017@users.noreply.github.com>
2025-05-03 13:13:31 +0300

    Form Register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,65 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import RegisterForm from "../forms/RegisterFom";
 
 const RegisterPage = () => {
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    navigate("/main");
-  };
   return (
-    <div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 via-white to-purple-100">
-      <div className="w-full max-w-md p-8 bg-white rounded-3xl shadow-xl space-y-6">
-        <h1 className="font-bold text-3xl text-gray-800 flex justify-center">
-          Kayıt Ol
-        </h1>
-
-        <form className="flex flex-col space-y-4" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Ad"
-            required
-            className="p-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Soyad"
-            required
-            className="p-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            onChange={(e) => setSurname(e.target.value)}
-          />
-          <input
-            type="email"
-            placeholder="E-posta"
-            required
-            className="p-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Şifre"
-            required
-            className="p-3 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 rounded-xl transition duration-200">
-            Kayıt Ol
-          </button>
-        </form>
-        <div className="text-center text-sm text-gray-600">
-          Zaten hesabın var mı?
-          <a href="/login" className="text-blue-600 hover:underline">
-            Giriş Yap
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-};
+      <RegisterForm/>
+    );
+  };
+ 
 export default RegisterPage;
